1b412548004f92e14aaf97c5adefdc61a922651f
kometsubu06 <ayaka.tai06@gmail.com>
2025-08-26 15:53:14 +0900

    userモデルのアソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { guardian: 0, teacher: 1, admin: 9 }, _default: :guardian
+
+  belongs_to :organization, optional: true
+  # 先行しておく関連（後で実体を足す想定。未定ならコメントアウト可）
+  # has_many :guardianships
+  # has_many :children, through: :guardianships
 end
