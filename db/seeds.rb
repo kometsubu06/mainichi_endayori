61cb34c41fea48a86a7ec573d51a19d9fd401a2e
kometsubu06 <ayaka.tai06@gmail.com>
2025-08-29 14:06:22 +0900

    seedを通さない方針にした

--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-case Rails.env
-when "development"
-  load Rails.root.join("db/seeds/development.rb")
-when "production"
-  load Rails.root.join("db/seeds/production.rb") if File.exist?(Rails.root.join("db/seeds/production.rb"))
-end
